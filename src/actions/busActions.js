9790d9f3e4a893784a96bbc22d2d6bf72a227315
ngahika01 <mykenjoroge01@gmail.com>
2022-04-13 14:13:59 +0300

    Added bus actions

---
+++ src/actions/busActions.js
@@ -0,0 +1,177 @@
+import axios from "axios";
+import { url } from "../config/url";
+import { logout } from "./userActions";
+import { BUS_CREATE_FAIL, BUS_CREATE_REQUEST, BUS_CREATE_SUCCESS, BUS_DELETE_FAIL, BUS_DELETE_REQUEST, BUS_DELETE_SUCCESS, BUS_GET_FAIL, BUS_GET_REQUEST, BUS_GET_SUCCESS, BUS_LIST_FAIL, BUS_LIST_REQUEST, BUS_LIST_SUCCESS, BUS_UPDATE_FAIL, BUS_UPDATE_REQUEST, BUS_UPDATE_SUCCESS } from "../constants/busConstants";
+
+export const createBus = (bus) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: BUS_CREATE_REQUEST });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.post(`${url}/buses`, bus, config);
+    dispatch({
+      type: BUS_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: BUS_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listBuses = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: BUS_LIST_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`${url}/buses`, config);
+
+    dispatch({
+      type: BUS_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === "Not authorized, token failed") {
+      dispatch(logout());
+    }
+    console.log(message);
+
+    dispatch({
+      type: BUS_LIST_FAIL,
+      payload: message,
+    });
+  }
+};
+
+export const getBus = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: BUS_GET_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`${url}/buses/${id}`, config);
+
+    dispatch({
+      type: BUS_GET_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === "Not authorized, token failed") {
+      dispatch(logout());
+    }
+    console.log(message);
+
+    dispatch({
+      type: BUS_GET_FAIL,
+      payload: message,
+    });
+  }
+};
+
+export const updateBus = (bus) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: BUS_UPDATE_REQUEST });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.put(
+      `${url}/buses/${bus._id}`,
+      bus,
+      config
+    );
+    dispatch({
+      type: BUS_UPDATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === "Not authorized, token failed") {
+      dispatch(logout());
+    }
+    dispatch({
+      type: BUS_UPDATE_FAIL,
+      payload: message,
+    });
+  }
+};
+
+export const deleteBus = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: BUS_DELETE_REQUEST,
+    });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    await axios.delete(`${url}/buses/${id}`, config);
+    dispatch({
+      type: BUS_DELETE_SUCCESS,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === "Not authorized, token failed") {
+      dispatch(logout());
+    }
+    dispatch({
+      type: BUS_DELETE_FAIL,
+      payload: message,
+    });
+  }
+};
